b4021e9fe00db7b1d513f28a849050dab327b8a6
sergio5990 <sergey-kruk>
2019-09-20 02:07:05 +0300

    example

---
+++ model/src/main/java/com/github/sergio5990/servlet/example/model/User.java
@@ -0,0 +1,37 @@
+package com.github.sergio5990.servlet.example.model;
+
+import java.util.Set;
+
+public class User {
+    private String firstName;
+    private String lastName;
+    private String phone;
+    private String email;
+
+    public User(String firstName, String lastName, String phone, String email) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.phone = phone;
+        this.email = email;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getId() {
+        return firstName + "_" + lastName;
+    }
+}
