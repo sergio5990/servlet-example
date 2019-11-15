2c8ccf797bbddbd7beae0777cc32fb952d7c1901
sergio5990 <sergey-kruk>
2019-11-15 21:09:49 +0300

    spring mvc

--- web/src/main/java/com/github/sergio5990/servlet/example/web/servlet/LogoutServlet.java
+++ web/src/main/java/com/github/sergio5990/servlet/example/web/servlet/LogoutServlet.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -20,6 +21,7 @@ public class LogoutServlet {
     private SecurityService securityService;
 
     @GetMapping("/logout")
+//    @RequestMapping(value = "/logout", method = RequestMethod.GET)
     public String doGet(HttpServletRequest rq) {
         rq.getSession().removeAttribute("authUser");
         rq.getSession().invalidate();
